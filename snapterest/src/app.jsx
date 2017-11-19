e5ee709323b621dfd0f3c5d7ebb3ba526bcbf668
Christopher Walsh <walshification@gmail.com>
2017-11-19 15:54:00 -0600

    creates interactive toggle button

--- snapterest/src/app.jsx
+++ snapterest/src/app.jsx
@@ -1,23 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import template from './index.html';
 
-class ReactClass extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHidden: false,
-    };
+class Heading extends React.Component {
+  constructor() {
+    super();
   }
   render() {
-    if (this.state.isHidden) {
+    if (this.props.isHidden) {
       return null;
     }
 
-    return React.createElement('h1', { className: 'header' }, 'React Component');
+    return (
+      <h1 className='title' key='title'>React Component</h1>
+    );
   }
 };
 
-const reactComponentElement = React.createElement(ReactClass);
-const reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Stateful React Component'
+    };
+  }
+
+
+  render() {
+    return (
+      <button className='btn btn-default'
+              onClick={() => this.props.onClick()}>
+        {this.state.title}
+      </button>
+    );
+  }
+}
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isHeaderHidden: false };
+  }
+
+  renderHeading() {
+    if (this.state.isHeaderHidden) {
+      return null;
+    }
+    return (
+      <Heading />
+    );
+  }
+
+  handleClick() {
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeading()}
+        <Button onClick={() => this.handleClick()} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Container />, document.getElementById('react-application'));
