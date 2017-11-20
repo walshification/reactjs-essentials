85d61476310a0598d49933e25ab43df939dc4e02
Chris Walsh <cwalsh@leapfrogonline.com>
2017-11-20 16:11:16 -0600

    builds out header with style and default text

--- snapterest/src/components/Header.jsx
+++ snapterest/src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+const headerStyle = {
+  fontSize: '16px',
+  fontWeight: '300',
+  display: 'inline-block',
+  margin: '20px 10px'
+};
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <h1 className='header'>{this.props.text}</h1>
+    return <h2 style={headerStyle}>{this.props.text}</h2>
   }
 }
+
+Header.defaultProps = {
+ text: 'Default header'
+};
 
 module.exports = Header;
