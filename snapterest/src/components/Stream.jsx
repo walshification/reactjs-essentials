c628635f6be349c30549633f584cc7c646175bec
Christopher Walsh <walshification@gmail.com>
2017-11-19 22:06:38 -0600

    builds out stream component

--- snapterest/src/components/Stream.jsx
+++ snapterest/src/components/Stream.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import SnapkiteStreamClient from 'snapkite-stream-client';
+
+import StreamTweet from './StreamTweet.jsx';
+import Header from './Header.jsx';
 
 class Stream extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweet: null
+    }
+  }
+
+  componentDidMount() {
+    SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+  }
+
+  componentWillUnmount() {
+    SnapkiteStreamClient.destroyStream();
+  }
+
+  handleNewTweet(tweet) {
+    this.setState({
+      tweet: tweet
+    });
+  }
+
   render() {
+    const tweet = this.state.tweet;
+    if (tweet) {
+      return (
+        <StreamTweet
+          tweet={tweet}
+          onAddTweetToCollection={this.props.onAddTweetToCollection} />
+      );
+    }
     return (
-      <div></div>
+      <Header text='Waiting for public photos from Twitter...' />
     );
   }
 }
