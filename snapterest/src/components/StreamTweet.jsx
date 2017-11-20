414409f21102c323aaf05c1cd6ce48c31eb89a79
Chris Walsh <cwalsh@leapfrogonline.com>
2017-11-20 15:59:54 -0600

    creates methods for stream tweet update lifecycle

--- snapterest/src/components/StreamTweet.jsx
+++ snapterest/src/components/StreamTweet.jsx
@@ -37,6 +37,47 @@ class StreamTweet extends React.Component {
     window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('[Snapterest] StreamTweet: 4. Running componentWillReceiveProps()');
+
+    const currentTweetLength = this.props.tweet.text.length;
+    const nextTweetLength = nextProps.tweet.text.length;
+    const isNumberOfCharactersIncreasing = (nextTweetLength > currentTweetLength);
+    let headerText;
+
+    this.setState({
+      numberOfCharactersIsIncreasing: isNumberOfCharactersIncreasing
+    });
+
+    if(isNumberOfCharactersIncreasing) {
+      headerText = 'Number of characters is increasing';
+    } else {
+      headerText = 'Latest public photo from Twitter';
+    }
+
+    this.setState({
+      headerText: headerText
+    });
+
+    window.snapterest.numberOfReceivedTweets++;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Snapterest] StreamTweet: 5. Running shouldComponentUpdate()');
+
+    return (nextProps.tweet.text.length > 1);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Snapterest] StreamTweet: 6. Running componentWillUpdate()');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[Snapterest] StreamTweet: 7. Running componentDidUpdate()');
+
+    window.snapterest.numberOfDisplayedTweets++;
+  }
+
   componentWillUnmount() {
     console.log('[Snapterest] StreamTweet: 8. Running componentWillUnmount()');
 
