be244c327a5235ca34706ffafa76f8ea12d2f1f7
Chris Walsh <cwalsh@leapfrogonline.com>
2017-11-20 17:06:59 -0600

    starts a collection

--- snapterest/src/components/Collection.jsx
+++ snapterest/src/components/Collection.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import CollectionControls from './CollectionControls.jsx';
+import Header from './Header.jsx';
+import TweetList from './TweetList.jsx';
 
 class Collection extends React.Component {
-  render() {
-    return (
-      <div></div>
+  createHtmlMarkupStringOfTweetList() {
+    const htmlString = ReactDOMServer.renderToStaticMarkup(
+      <TweetList tweets={this.props.tweets} />
     );
+
+    const htmlMarkup = {
+      html: htmlString
+    };
+
+    return JSON.stringify(htmlMarkup);
+  }
+
+  getListOfTweetIds() {
+    return Object.keys(this.props.tweets);
+  }
+
+  getNumberOfTweetsInCollection() {
+    return this.getListOfTweetIds().length;
+  }
+
+  render() {
+    const numberOfTweetsInCollection = this.getNumberOfTweetsInCollection();
+
+    if(numberOfTweetsInCollection > 0) {
+      const tweets = this.props.tweets;
+      const htmlMarkup = this.createHtmlMarkupStringOfTweetList();
+      const removeAllTweetsFromCollection = this.props.onRemoveAllTweetsFromCollection;
+      const handleRemoveTweetFromCollection = this.props.onRemoveTweetFromCollection;
+
+      return (
+        <CollectionControls
+          numberOfTweetsInCollection={numberOfTweetsInCollection}
+          htmlMarkup={htmlMarkup}
+          onRemoveAllTweetsFromCollection={removeAllTweetsFromCollection} />
+
+          <TweetList
+            tweets={tweets}
+            onRemoveTweetFromCollection={handleRemoveTweetFromCollection} />
+      );
+    }
+
+    return <Header text="Your collection is empty" />;
   }
 }
 
