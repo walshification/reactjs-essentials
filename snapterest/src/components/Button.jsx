047059a0aa9421deb7cc16865d631e3f8fdc6657
Christopher Walsh <walshification@gmail.com>
2017-11-20 20:59:50 -0600

    creates button

---
+++ snapterest/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const buttonStyle = {
+  margin: '10px 10px 10px 0'
+};
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className='btn btn-default'
+        style={buttonStyle}
+        onClick={this.props.handleClick}>
+        {this.props.label}
+      </button>
+    );
+  }
+}
+
+module.exports = Button;
